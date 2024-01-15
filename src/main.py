ccefd5885fe38dd658467b46945ddf6622741535
Henrique Roberto Gattermann Mittelstaedt <henrique.roberto97@gmail.com>
2024-01-15 18:58:28 -0300

    Adicionada opção dezenas

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import argparse
 
 
-from loterias import opcoes_loterias
+from loterias import opcoes_loterias, loteria_factory
 
 
 def main(args):
     parser = argparse.ArgumentParser(prog='loteria')
     parser.add_argument('loteria', choices=opcoes_loterias)
+    parser.add_argument('-d', '--dezenas', type=int, nargs='*')
     args = parser.parse_args(args.split())
 
-    loteria = opcoes_loterias[args.loteria]
+    loteria = loteria_factory(args.loteria)
+    
+    aposta = loteria.criar_aposta(args.dezenas)
+    if aposta:
+        print(aposta.dezenas)
+
     loteria._adb.close_db()
 
 if __name__ == '__main__':
-    main('mega')
+    main('mega -d 2 13 17 44 47 65')
