5e98680a92c25ccde0f2a4dbfcee371670b13a20
Henrique Roberto Gattermann Mittelstaedt <henrique.roberto97@gmail.com>
2024-01-16 10:14:41 -0300

    Implementada classe Resultado

--- src/db_manager.py
+++ src/db_manager.py
@@ -80,3 +80,115 @@ class ApostaDB(DBManager):
             sql, [arg for arg in (loteria, concurso) if arg])
         apostas = [Aposta.from_db(ap) for ap in querry.fetchall()]
         return apostas
+
+
+class ResultadoDB(DBManager):
+    '''Gerenciador do banco de dados dos resultados das Loterias Caixa.'''
+    nome_tabela = 'resultados'
+
+    def __init__(self, db_path: str = None) -> None:
+        if db_path is None:
+            db_path = f'{self.nome_tabela}.db'
+        super().__init__(db_path)
+        self.criar_tabela()
+
+    def criar_tabela(self) -> None:
+        '''Cria a tabela resultados no banco de dados.'''
+        sql = f'''CREATE TABLE IF NOT EXISTS {self.nome_tabela} (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            acumulou INTEGER,
+            concurso INTEGER,
+            data TEXT,
+            dataProximoConcurso TEXT,
+            dezenas TEXT,
+            dezenasOrdemSorteio TEXT,
+            estadosPremiados TEXT,
+            local TEXT,
+            localGanhadores TEXT,
+            loteria TEXT,
+            mesSorte TEXT,
+            observacao TEXT,
+            premiacoes TEXT,
+            proximoConcurso INTEGER,
+            timeCoracao TEXT,
+            trevos TEXT,
+            valorAcumuladoConcursoEspecial REAL,
+            valorAcumuladoConcurso_0_5 REAL,
+            valorAcumuladoProximoConcurso REAL,
+            valorArrecadado REAL,
+            valorEstimadoProximoConcurso INTEGER,
+            UNIQUE(loteria, concurso))'''
+        self.cursor.execute(sql)
+        self.commit_db()
+        logger.debug('Tabela %s criada com sucesso!', self.nome_tabela)
+
+    def registrar_resultado(self, resultado: dict) -> None:
+        '''Registra um novo resultado no banco de dados'''
+        sql = f'''INSERT INTO {self.nome_tabela} (
+            acumulou,
+            concurso,
+            data,
+            dataProximoConcurso,
+            dezenas,
+            dezenasOrdemSorteio,
+            estadosPremiados,
+            local,
+            localGanhadores,
+            loteria,
+            mesSorte,
+            observacao,
+            premiacoes,
+            proximoConcurso,
+            timeCoracao,
+            trevos,
+            valorAcumuladoConcursoEspecial,
+            valorAcumuladoConcurso_0_5,
+            valorAcumuladoProximoConcurso,
+            valorArrecadado,
+            valorEstimadoProximoConcurso) 
+                VALUES (
+            :acumulou,
+            :concurso,
+            :data,
+            :dataProximoConcurso,
+            :dezenas,
+            :dezenasOrdemSorteio,
+            :estadosPremiados,
+            :local,
+            :localGanhadores,
+            :loteria,
+            :mesSorte,
+            :observacao,
+            :premiacoes,
+            :proximoConcurso,
+            :timeCoracao,
+            :trevos,
+            :valorAcumuladoConcursoEspecial,
+            :valorAcumuladoConcurso_0_5,
+            :valorAcumuladoProximoConcurso,
+            :valorArrecadado,
+            :valorEstimadoProximoConcurso)'''
+        try:
+            self.cursor.execute(sql, resultado.to_db())
+            self.commit_db()
+            logger.info('Registro inserido com sucesso em "%s"!', self.nome_tabela)
+        except sqlite3.IntegrityError:
+            logger.debug('Registro já existe, nada para fazer.')
+
+    def ler_todos_resultados(self) -> list:
+        logger.debug('Lendo todos os resultados...')
+        querry = self.cursor.execute(f'SELECT * FROM {self.nome_tabela}').fetchall()
+        return [Resultado.from_db(r) for r in querry]
+
+    def ler_resultados_por_loteria(self, loteria: str, concurso: int = None) -> list:
+        logger.debug(f'Lendo resultados da {loteria}. Concurso: {concurso if concurso else "TODOS"}...')
+        filtros = f'(loteria, concurso) = ("{loteria}", {concurso})' if concurso else f'loteria = "{loteria}"'
+        querry = self.cursor.execute(f'SELECT * FROM resultados WHERE {filtros}').fetchall()
+        return [Resultado.from_db(r) for r in querry]
+
+    def ultimo_concurso_resultado_registrado_por_loteria(self, loteria: str):
+        logger.debug(f'Buscando último concurso registrado da {loteria}.')
+        sql = f'SELECT MAX(concurso),* FROM {self.nome_tabela} WHERE loteria = "{loteria}"'
+        querry = self.cursor.execute(sql).fetchone()
+        concurso, *resultado = querry
+        return concurso, Resultado.from_db(resultado)
---
+++ src/resultado.py
@@ -0,0 +1,82 @@
+import json
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Resultado:
+    '''Retorna um Resultado das Loterias Caixa.'''
+    acumulou: bool
+    concurso: int
+    data: str
+    dataProximoConcurso: str
+    dezenas: list[str]
+    dezenasOrdemSorteio: list[str]
+    estadosPremiados: list
+    local: str
+    localGanhadores: list
+    loteria: str
+    mesSorte: str
+    observacao: str
+    premiacoes: list[dict]
+    proximoConcurso: int
+    timeCoracao: str
+    trevos: list[str]
+    valorAcumuladoConcursoEspecial: float
+    valorAcumuladoConcurso_0_5: float
+    valorAcumuladoProximoConcurso: float
+    valorArrecadado: float
+    valorEstimadoProximoConcurso: int
+    _id: int = None
+
+    def to_db(self) -> dict:
+        '''Exporta como dicionário para salvar no Banco de Dados.'''
+        return {'acumulou': self.acumulou,
+                'concurso': self.concurso, 
+                'data': self.data, 
+                'dataProximoConcurso': self.dataProximoConcurso, 
+                'dezenas': json.dumps(self.dezenas), 
+                'dezenasOrdemSorteio': json.dumps(self.dezenasOrdemSorteio), 
+                'estadosPremiados': json.dumps(self.estadosPremiados), 
+                'local': self.local, 
+                'localGanhadores': json.dumps(self.localGanhadores), 
+                'loteria': self.loteria, 
+                'mesSorte': self.mesSorte, 
+                'observacao': self.observacao, 
+                'premiacoes': json.dumps(self.premiacoes), 
+                'proximoConcurso': self.proximoConcurso, 
+                'timeCoracao': self.timeCoracao, 
+                'trevos': json.dumps(self.trevos), 
+                'valorAcumuladoConcursoEspecial': self.valorAcumuladoConcursoEspecial, 
+                'valorAcumuladoConcurso_0_5': self.valorAcumuladoConcurso_0_5, 
+                'valorAcumuladoProximoConcurso': self.valorAcumuladoProximoConcurso, 
+                'valorArrecadado': self.valorArrecadado, 
+                'valorEstimadoProximoConcurso': self.valorEstimadoProximoConcurso}
+
+    @classmethod
+    def from_db(cls, resultado):
+        '''Retorna um Resultado a partir de uma querry do Banco de Dados.'''
+        if resultado: 
+            return Resultado(
+                _id= resultado[0],
+                acumulou = bool(resultado[1]),
+                concurso = resultado[2],
+                data = resultado[3],
+                dataProximoConcurso = resultado[4],
+                dezenas = json.loads(resultado[5]),
+                dezenasOrdemSorteio = json.loads(resultado[6]),
+                estadosPremiados = json.loads(resultado[7]),
+                local = resultado[8],
+                localGanhadores = json.loads(resultado[9]),
+                loteria = resultado[10],
+                mesSorte = resultado[11],
+                observacao = resultado[12],
+                premiacoes = json.loads(resultado[13]),
+                proximoConcurso = resultado[14],
+                timeCoracao = resultado[15],
+                trevos = json.loads(resultado[16]),
+                valorAcumuladoConcursoEspecial = resultado[17],
+                valorAcumuladoConcurso_0_5 = resultado[18],
+                valorAcumuladoProximoConcurso = resultado[19],
+                valorArrecadado = resultado[20],
+                valorEstimadoProximoConcurso = resultado[21],)
