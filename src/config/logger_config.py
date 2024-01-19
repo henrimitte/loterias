bc93f5791a2bdb94c031c6f9c84d992e4fc91b1d
Henrique Roberto Gattermann Mittelstaedt <henrique.roberto97@gmail.com>
2024-01-19 12:36:06 -0300

    Adicionado log em arquivo

--- src/config/logger_config.py
+++ src/config/logger_config.py
@@ -1,11 +1,19 @@
 import logging
 
+from .settings import LOGS_DIR
 
-def config_logger(name: str) -> logging.Logger:
+
+def config_logger(name: str, file: bool = False) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
-    formatter = logging.Formatter('[%(name)s] [%(levelname)s] %(message)s')
+    formatter = logging.Formatter('[%(levelname)s] [%(name)s] %(message)s')
+
+    log_filename = LOGS_DIR / f'{name}.log'
+    file_handler = logging.FileHandler(filename=log_filename)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.DEBUG)
